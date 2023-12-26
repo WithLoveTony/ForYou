848e7677c61d8991cec0f94ba7ab179c5761e4a2
Toni lv <154926734+WithLoveTony@users.noreply.github.com>
2023-12-26 17:14:37 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,3 +19,17 @@ function moveToFirst(cardNumber) {
     var translateValue = -((160 + 0) * index); // Largura do card mais a margem
     carousel.style.transform = 'translateX(' + translateValue + 'px)';
 }
+
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    } else {
+        cancelFullScreen.call(doc);
+    }
+}
