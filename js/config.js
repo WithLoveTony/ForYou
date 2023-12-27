45ed349dcd08698baf3c2fb97be0b9747bf0bf8e
Toni lv <154926734+WithLoveTony@users.noreply.github.com>
2023-12-27 13:39:10 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -20,7 +20,7 @@ function moveToFirst(cardNumber) {
     if(window.innerHeight<800){
         var translateValue = -((170 + 0) * index); // Largura do card mais a margem
     }else{
-        var translateValue = -((210 + 0) * index); // Largura do card mais a margem
+        var translateValue = -((200 + 0) * index); // Largura do card mais a margem
     }
     carousel.style.transform = 'translateX(' + translateValue + 'px)';
     update(cardNumber-1)
