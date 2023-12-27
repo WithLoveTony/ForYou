9849e0ca4e6ffaa69151df992d82cad1c5ed798b
Toni lv <154926734+WithLoveTony@users.noreply.github.com>
2023-12-27 00:50:40 -0300

    Add files via upload

--- js/information.js
+++ js/information.js
@@ -5,28 +5,28 @@
 // 
 const information = {
   MTTH3S: {
-    nome: "Matheus",
+    nome: "Teteu",
     fotoCard: ["https://pbs.twimg.com/media/GCSyoxIWEAAJYyE?format=jpg&name=4096x4096", "https://pbs.twimg.com/media/GCUQ042WcAAwjOT?format=jpg&name=4096x4096", 'https://pbs.twimg.com/media/GCS0K3dWQAAsQgx?format=jpg&name=large', 'https://pbs.twimg.com/media/GCS0LDpWoAAGJO5?format=jpg&name=large', 'https://pbs.twimg.com/media/GCS0KqNWQAAF2GT?format=jpg&name=large'],
     nomeCard: ["nome1", "nome2", "nome2", '', ''],
     textoCard: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus volutpat lorem vitae auctor. Nam quam erat, faucibus ac justo non, dignissim luctus velit. Proin convallis nibh sit amet nibh porta interdum. Quisque pharetra aliquet luctus. Sed ac euismod dolor, et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.", "texto2", "nome2"],
     cor: ['32', '174', '220', '200', '10']
   },
   TRR3S: {
-    nome: "Júlia",
+    nome: "Julinha",
     fotoCard: ["https://pbs.twimg.com/media/GCUNuKtWsAAVl6H?format=jpg&name=4096x4096", "https://pbs.twimg.com/media/GCUNtzUXYAAThIf?format=jpg&name=large", 'https://pbs.twimg.com/media/GCUPCUFXQAAn3rr?format=jpg&name=large', 'https://pbs.twimg.com/media/GCUPCIwXEAATNUE?format=jpg&name=large'],
     nomeCard: ["Lorem Ipsum", "nome2", 'título 3', 't4'],
     textoCard: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus volutpat lorem vitae auctor. Nam quam erat, faucibus ac justo non, dignissim luctus velit. Proin convallis nibh sit amet nibh porta interdum. Quisque pharetra aliquet luctus. Sed ac euismod dolor, et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.", "texto2", '00', '00'],
     cor: ['210', '20', '12', '40']
   },
   RYH0SE: {
-    nome: "Irys Rhose",
+    nome: "Tia Rhose",
     fotoCard: ["https://pbs.twimg.com/media/GCUHx17XAAAEFB7?format=jpg&name=medium", "https://pbs.twimg.com/media/GCUIG4VX0AAFCgl?format=png&name=small", 'https://pbs.twimg.com/media/GCUHeMUX0AAqhm_?format=png&name=small'],
     nomeCard: ["Rhose", "Irys", 'Card'],
     textoCard: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus volutpat lorem vitae auctor. Nam quam erat, faucibus ac justo non, dignissim luctus velit. Proin convallis nibh sit amet nibh porta interdum. Quisque pharetra aliquet luctus. Sed ac euismod dolor, et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.", "texto2", 'algo'],
     cor: ['20', '352', '30']
   },
   C4RLTS: {
-    nome: "Carlos Vinícius",
+    nome: "Carlitos",
     fotoCard: ["https://pbs.twimg.com/media/GCUWMB8WsAAqQZj?format=jpg&name=large", 'https://pbs.twimg.com/media/GCUWL3SWIAAFgmX?format=jpg&name=large', 'https://pbs.twimg.com/media/GCUYMTtWoAA-odv?format=jpg&name=small'],
     nomeCard: ["Carlitos", "tt", 'Card'],
     textoCard: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus volutpat lorem vitae auctor. Nam quam erat, faucibus ac justo non, dignissim luctus velit. Proin convallis nibh sit amet nibh porta interdum. Quisque pharetra aliquet luctus. Sed ac euismod dolor, et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.et rutrum mi. Maecenas consectetur, quam sed pellentesque varius, diam tellus dictum diam, quis tristique libero urna at magna. Etiam mattis auctor libero eu posuere.", "texto2", 'algo'],
